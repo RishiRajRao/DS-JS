3d76dee52136db6c46e911bea6e279872f7e3900
RishiRajRao <76801849+RishiRajRao@users.noreply.github.com>
2024-02-08 15:17:33 +0530

    Update reverseString.js
    reverse with split,map,join

--- reverseString.js
+++ reverseString.js
@@ -1,29 +1,50 @@
-const reverseWord = str => {
-    let tempStr = "",resultStr = "";
-    str  = str + " "
-    console.log("1",str.length)
-    for(let val of str){
-      if(val.charCodeAt(0)== 32){
-        resultStr += reverseString(tempStr) + " "
-        tempStr = ""
-        continue;
-      }
-      tempStr += val
-      // if(val.charCodeAt(0)== 32){
-      //   resultStr += reverseString(tempStr) + " "
-      // }
-    }
-    console.log("2",resultStr.length)
-    return resultStr;
+// const reverseWord = str => {
+//     let tempStr = "",resultStr = "";
+//     str  = str + " "
+//     console.log("1",str.length)
+//     for(let val of str){
+//       if(val.charCodeAt(0)== 32){
+//         resultStr += reverseString(tempStr) + " "
+//         tempStr = ""
+//         continue;
+//       }
+//       tempStr += val
+//       // if(val.charCodeAt(0)== 32){
+//       //   resultStr += reverseString(tempStr) + " "
+//       // }
+//     }
+//     console.log("2",resultStr.length)
+//     return resultStr;
+// }
+
+// const reverseString = str => {
+//   let temp = "";
+//   for(j = str.length-1;j>=0;j--){
+//     temp +=str[j]
+//   }
+//   return temp;
+// }
+
+// // console.log(resultStr(Welcome to this Javascript Guide!))
+// console.log(reverseWord("Welcome to this Javascript Guide!"))
+
+
+
+const reverseWords = str => {
+	const words  = str.split(' ').map(word=>reverse(word)).join(' ')
+  console.log('words:' , words)
+  return;
 }
 
-const reverseString = str => {
-  let temp = "";
-  for(j = str.length-1;j>=0;j--){
-    temp +=str[j]
+const reverse = word => {
+	let i = 0 , j = word.length-1 ,str = ''
+  while(j>=0){
+  	str += word[j]
+    j--
   }
-  return temp;
+  console.log('rev:',str)
+  return str
 }
 
-// console.log(resultStr(Welcome to this Javascript Guide!))
-console.log(reverseWord("Welcome to this Javascript Guide!"))
+
+reverseWords('Welcome to Game Begins')
