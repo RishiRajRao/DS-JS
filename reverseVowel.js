0b18f05cffc5e9468549b4dd898dbd13227b2ad4
RishiRajRao <76801849+RishiRajRao@users.noreply.github.com>
2024-02-12 18:43:44 +0530

    Update reverseVowel.js

--- reverseVowel.js
+++ reverseVowel.js
@@ -1,4 +1,40 @@
-const str = "abcehijkosdfu"
+// const str = "abcehijkosdfu"
+
+// const isVowel = val => {
+//   let vowelArr = ['a','e','i','o','u']
+//   if(vowelArr.includes(val)){
+//     return true;
+//   }else{
+//     return false;
+//   }
+// }
+
+// const swap = (a,b,str) => {
+//   var arr = str.split("")
+//   let temp = arr[a]
+//   arr[a] = arr[b]
+//   arr[b] = temp
+//   str = arr.join("")
+//   return str;
+// }
+
+// const reverseVowel = str => {
+//   let i = 0,j = str.length-1;
+//   while(i!=j) {
+//     if(isVowel(str[i]) && isVowel(str[j])){
+//         str = swap(i,j,str);
+//         i++,j--;
+//     }
+//     if(!isVowel(str[i]))i++;
+//     if(!isVowel(str[j]))j--;
+//   }
+//   return str;
+// }
+
+// console.log(reverseVowel(str))
+
+//updated
+const sample = "abcehijkosdfu"
 
 const isVowel = val => {
   let vowelArr = ['a','e','i','o','u']
@@ -9,29 +45,33 @@ const isVowel = val => {
   }
 }
 
-const swap = (a,b,str) => {
-  var arr = str.split("")
-  let temp = arr[a]
-  arr[a] = arr[b]
-  arr[b] = temp
-  str = arr.join("")
-  return str;
-}
-
 const reverseVowel = str => {
-  let i = 0,j = str.length-1;
-  while(i!=j) {
-    if(isVowel(str[i]) && isVowel(str[j])){
-        str = swap(i,j,str);
-        i++,j--;
+	let idxBox = [] ,reversed = ''
+  for(let i = 0; i<str.length;i++){
+  	if(isVowel(str[i])){
+    	idxBox.push(str[i])
     }
-    if(!isVowel(str[i]))i++;
-    if(!isVowel(str[j]))j--;
   }
-  return str;
+
+for(let val of str){
+	if(isVowel(val)){
+  	reversed += idxBox.pop()
+  }else{
+  	reversed +=val
+  }
 }
 
-console.log(reverseVowel(str))
+console.log('revStr', reversed)
+
+}
+
+reverseVowel(sample)
+
+
+
+
+
+
 
 
 
